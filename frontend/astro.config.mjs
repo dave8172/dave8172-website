1825e7f46ab23569f3e3c88a1b91ddecdebd5af6
Dhawal <dhawal.1294@gmail.com>
2026-02-03 16:14:18 +0530

    added sitemap and excluded /pvt/ folder

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,8 +1,15 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  integrations: [mdx()],
+  site: "https://dave8172-website.vercel.app",
+  integrations: [
+    mdx(),
+    sitemap({
+      filter: (page) => !page.includes("/pvt/")
+    })
+  ],
   markdown: {
     shikiConfig: {
       theme: 'one-dark-pro',
